eb891a47c0fb24dd643a2122af7e0896aa123e37
Masoud <msmsd778@gmail.com>
2021-04-21 20:25:04 +0430

    almost finished the backend & just started the design

--- base/urls.py
+++ base/urls.py
@@ -5,6 +5,7 @@ from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('login/', views.MyLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('register/', views.RegisterPage.as_view(), name='register'),
     path('', views.TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', views.TaskDetail.as_view(), name='task'),
     path('task-create/', views.TaskCreate.as_view(), name='task-create'),
