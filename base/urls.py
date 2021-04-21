5dd0972ee53b77cc2a6d96e7f474439b53eaa4b3
Masoud <msmsd778@gmail.com>
2021-04-21 17:02:03 +0430

    created the login & logout + tasks privacy

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
+    path('login/', views.MyLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('', views.TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', views.TaskDetail.as_view(), name='task'),
     path('task-create/', views.TaskCreate.as_view(), name='task-create'),
